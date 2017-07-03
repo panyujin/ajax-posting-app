485d3b3c9be78c388d83aca5e03a7437497b5dec
panyujin <panyujinyx@gmail.com>
2017-07-03 12:42:45 +0800

    add bootstrap to project

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get "/jquery-1" => "pages#jquery_1"
+  get "/jquery-2" => "pages#jquery_2"
+  get "/jquery-3" => "pages#jquery_3"
+  get "/jquery-4" => "pages#jquery_4"
+  get "/jquery-5" => "pages#jquery_5"
+
+  resources :posts
+
+  root "posts#index"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
